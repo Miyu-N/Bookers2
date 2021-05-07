d745b6aef454ca05bb541cf43086d5c23f97cf16
Miyu-N <myu.star1217@gmail.com>
2021-05-07 14:21:39 +0000

    [Add]UsersController追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,19 @@
 class BooksController < ApplicationController
   
   def create
+    @book = book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
+    @books = Book.all
   end
 
   def edit
@@ -18,5 +25,8 @@ class BooksController < ApplicationController
   def destroy
   end
   
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end  
   
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  
+  def new
+    @book = Book.new
+  end
+  
+  def show
+    @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books
+  end 
+  
+  def new
+    @user = User.new(user_params)
+    @user.save
+    redirect_to user_path(current_user.id)
+  end 
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.all
+    @books =Book.all
+    @book = Book.new
+    @user = current_user
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to "/users/#{current_user.id}"
+  end
+  
+  private
+  
+  def book_params
+      params.require(:book).permit(:title, :body)
+  end
+
+  def user_params
+      params.require(:user).permit(:name, :profile_image, :introduction)
+  end 
+  
+  
+  
+  
+end
