3776266f5352344e51b3152a1ccd3adf08994680
Miyu-N <myu.star1217@gmail.com>
2021-05-12 10:18:57 +0000

    [RSpecテスト]

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
- root 'homes#top'
-  
- 
- get 'users' => 'users#index'
- get 'home/about' => 'homes#about'
- 
- resources :books
+  devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }
 
- resources :users,only:[:show,:edit,:update]
- 
+  root "homes#top"
+
+  get "/home/about" => "homes#about"
+  resources :users
+  resources :books
 
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -28,9 +28,9 @@ ActiveRecord::Schema.define(version: 2021_05_11_123117) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "introduction"
     t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
